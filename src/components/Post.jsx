4f676795a06ffd9611dc5eff17547cadfa22abd4
Danilo <danilofossatto2011@hotmail.com>
2023-08-06 22:45:35 -0300

    adcionado campo 'comentar'

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -28,6 +28,13 @@ export function Post(props){
           <a href=''>#rocketseat</a>
         </p>
         </div>
+        <form className={styles.commentForm}>
+            <strong>Deixe seu feedback</strong>
+            <textarea placeholder='Deixe um comentário'></textarea>
+            <footer>
+                <button type='submit'>Publicar</button>
+            </footer>
+        </form>
         </article>
     )
 }
